5c88928c4f1f54a3d0e0b21783ede088bce0dc67
Howard.Zuo <leftstick@qq.com>
2014-03-25 17:50:45 +0800

    add minified version of css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var cssminify = require('gulp-minify-css');
 
 
 
 gulp.task('default', function() {
-    return gulp.src('jquery.onepage-scroll.js').pipe(uglify()).pipe(rename({
+    gulp.src('jquery.onepage-scroll.js').pipe(uglify()).pipe(rename({
         extname: ".min.js"
     })).pipe(gulp.dest('./'));
+
+    gulp.src('onepage-scroll.css').pipe(cssminify()).pipe(rename({
+        extname: ".min.css"
+    })).pipe(gulp.dest('./'));
+
 });
